fc84fcb4be9085e405dc1dd0902768c70b0a155e
Ziterz <ziady.mubaraq@gmail.com>
2023-03-11 15:41:41 +0700

    add skeleton loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,38 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 function Card(props) {
   return (
     <div className={`flex justify-center items-center mt-5 && ${props.bg}`}>
       <div className="flex-row">
-        <img
-          src={props.data.picture}
-          alt="picture"
-          className="block mx-auto rounded-full w-40 h-40"
-        />
+        {props.data.picture ? (
+          <img
+            src={props.data.picture}
+            alt="picture"
+            className="block mx-auto rounded-full w-40 h-40 object-cover"
+          />
+        ) : (
+          <Skeleton height={160} width={160} />
+        )}
+
         <h2 className="text-xl mt-5 mb-2 font-semibold">
-          {props.data.name ? props.data.name : "-"}
+          {props.data.name || <Skeleton duration={1.5} />}
         </h2>
         <span className="text-sm text-slate-400">Background :</span>
-        <h2 className="text-lg mb-2 text-slate-900">
-          {props.data.background ? props.data.background : "-"}
+        <h2 className="text-md mb-2 text-slate-900">
+          {props.data.background || <Skeleton />}
         </h2>
         <span className="text-sm text-slate-400">Interest :</span>
-        <h2 className="text-lg mb-2 text-slate-900">
-          {props.data.interest ? props.data.interest : "-"}
+        <h2 className="text-md mb-2 text-slate-900">
+          {props.data.interest || <Skeleton />}
         </h2>
         <span className="text-sm text-slate-400">Goals :</span>
-        <h2 className="text-lg mb-2 text-slate-900">
-          {props.data.goals ? props.data.goals : "-"}
+        <h2 className="text-md mb-2 text-slate-900">
+          {props.data.goals || <Skeleton />}
         </h2>
         <span className="text-sm text-slate-400">Social Media :</span>
-        <h2 className="text-lg mb-2 text-slate-900">
-          {props.data.socialMedia ? props.data.socialMedia : "-"}
+        <h2 className="text-md mb-2 text-slate-900">
+          {props.data.socialMedia || <Skeleton />}
         </h2>
         <div className="mt-10">
           <i className="fa-solid fa-unlock text-2xl"></i>
