a9783f9f2d91d5e2db8b256f1946caf4284e6352
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2023-03-14 08:54:13 +0700

    fix layout

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -21,14 +21,14 @@ function OurTeam() {
     <div className="mx-auto mb-4">
       <div className="w-screen px-5 md:px-20 py-4 bg-white flex justify-between items-center">
         <Link
-          className="flex-col hover:underline focus:underline hover:underline-offset-4 focus:underline-offset-4"
+          className="flex-col hover:underline focus:underline hover:underline-offset-4 focus:underline-offset-4 2xl:mt-5 2xl:text-xl"
           to="/"
         >
           <i class="fa-solid fa-arrow-left mr-2"></i> Back to Home
         </Link>
       </div>
       <div className="w-full md:w-2/3 text-center md:text-left md:px-20 px-5">
-        <h1 className="text-5xl md:text-6xl md:mt-20 font-semibold">
+        <h1 className="text-5xl md:text-6xl 2xl:text-8xl md:mt-20 font-semibold 2xl:mt-48">
           🙌 <br />
           Meet our team of <br />
           developers, designers
@@ -36,7 +36,7 @@ function OurTeam() {
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-5 mt-10 md:gap-24 px-20">
+      <div className="grid grid-cols-1 lg:grid-cols-5 mt-10 md:gap-24 px-20 2xl:mt-48">
         <div>
           {loading ? (
             <Skeleton circle={true} height={192} width={192} />
@@ -44,17 +44,17 @@ function OurTeam() {
             <img
               src={ziadyPicture}
               alt="picture"
-              className="block text-center mx-auto w-52 h-52 object-cover"
+              className="block text-center mx-auto w-52 h-52 object-cover 2xl:w-60 2xl:h-60"
               onError={(e) => {
                 e.target.onerror = null;
                 e.target.src = defaultProfilePicture;
               }}
             />
           )}
-          <h2 className="text-2xl mt-2 ml-2 font-semibold">
+          <h2 className="text-2xl mt-2 ml-2 font-semibold 2xl:text-3xl">
             {loading ? <Skeleton /> : "Ziady"}
           </h2>
-          <h3 className="mb-2 ml-2 text-lg">
+          <h3 className="mb-2 ml-2 text-lg 2xl:text-xl">
             {loading ? <Skeleton /> : "Developer"}
           </h3>
         </div>
@@ -65,17 +65,17 @@ function OurTeam() {
             <img
               src={safikPicture}
               alt="picture"
-              className="block text-center mx-auto w-52 h-52 object-cover"
+              className="block text-center mx-auto w-52 h-52 object-cover 2xl:w-60 2xl:h-60"
               onError={(e) => {
                 e.target.onerror = null;
                 e.target.src = defaultProfilePicture;
               }}
             />
           )}
-          <h2 className="text-2xl mt-2 ml-2 font-semibold">
+          <h2 className="text-2xl mt-2 ml-2 font-semibold 2xl:text-3xl">
             {loading ? <Skeleton /> : "Safik"}
           </h2>
-          <h3 className="mb-2 ml-2 text-lg">
+          <h3 className="mb-2 ml-2 text-lg 2xl:text-xl">
             {loading ? <Skeleton /> : "Roamer"}
           </h3>
         </div>
@@ -86,17 +86,17 @@ function OurTeam() {
             <img
               src={fajriPicture}
               alt="picture"
-              className="block text-center mx-auto w-52 h-52 object-cover"
+              className="block text-center mx-auto w-52 h-52 object-cover 2xl:w-60 2xl:h-60"
               onError={(e) => {
                 e.target.onerror = null;
                 e.target.src = defaultProfilePicture;
               }}
             />
           )}
-          <h2 className="text-2xl mt-2 ml-2 font-semibold">
+          <h2 className="text-2xl mt-2 ml-2 font-semibold 2xl:text-3xl">
             {loading ? <Skeleton /> : "Fajri"}
           </h2>
-          <h3 className="mb-2 ml-2 text-lg">
+          <h3 className="mb-2 ml-2 text-lg 2xl:text-xl">
             {loading ? <Skeleton /> : "Developer"}
           </h3>
         </div>
@@ -107,17 +107,17 @@ function OurTeam() {
             <img
               src={zidanPicture}
               alt="picture"
-              className="block text-center mx-auto w-52 h-52 object-cover"
+              className="block text-center mx-auto w-52 h-52 object-cover 2xl:w-60 2xl:h-60"
               onError={(e) => {
                 e.target.onerror = null;
                 e.target.src = defaultProfilePicture;
               }}
             />
           )}
-          <h2 className="text-2xl mt-2 ml-2 font-semibold">
+          <h2 className="text-2xl mt-2 ml-2 font-semibold 2xl:text-3xl">
             {loading ? <Skeleton /> : "Zidan"}
           </h2>
-          <h3 className="mb-2 ml-2 text-lg">
+          <h3 className="mb-2 ml-2 text-lg 2xl:text-xl">
             {loading ? <Skeleton /> : "Offlaner"}
           </h3>
         </div>
@@ -128,18 +128,18 @@ function OurTeam() {
             <img
               src={wahyuPicture}
               alt="picture"
-              className="block text-center mx-auto w-52 h-52 object-cover"
+              className="block text-center mx-auto w-52 h-52 object-cover 2xl:w-60 2xl:h-60"
               onError={(e) => {
                 e.target.onerror = null;
                 e.target.src = defaultProfilePicture;
               }}
             />
           )}
-          <h2 className="text-2xl mt-2 ml-2 font-semibold">
+          <h2 className="text-2xl mt-2 ml-2 font-semibold 2xl:text-3xl">
             {loading ? <Skeleton /> : "Wahyu"}
           </h2>
-          <h3 className="mb-2 ml-2 text-lg">
-            {loading ? <Skeleton /> : "Developer"}
+          <h3 className="mb-2 ml-2 text-lg 2xl:text-xl">
+            {loading ? <Skeleton /> : "Support"}
           </h3>
         </div>
       </div>
